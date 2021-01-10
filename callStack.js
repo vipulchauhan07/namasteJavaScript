9368286b2cccf18e071a7dd923c62a25d2565178
Vipul Chauhan <viratdev07@gmail.com>
2021-01-10 14:07:03 +0530

    CallStack Execution

---
+++ callStack.js
@@ -0,0 +1,13 @@
+var n  = 2;
+
+function square(num) {
+    var ans = num * num;
+    return ans;
+}
+
+var square2 = square(n);
+var square4 = square(4);
+
+console.log("*********** Ans is ***********");
+console.log("Result of square of 2: " + square2);
+console.log("Result of square of 4: " + square4);
