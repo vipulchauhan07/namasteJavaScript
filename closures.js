51a1bf00c2f8b667809c8b58e1423618d05a320c
Vipul Chauhan <viratdev07@gmail.com>
2021-01-11 06:46:35 +0530

    One more level deep into closures

--- closures.js
+++ closures.js
@@ -1,25 +1,45 @@
-function x() {
-    var a = 10;
-    function y() {
-        console.log(a);
-    }    
-    return y;
-}
-
-var z = x();
-console.log(z);
-z();
+// // Def: Closure in JavaScript is nothing but a function return with it's lexical environment.
 
 
-// Above function is same as --->
 
-function x() {
-    var a = 10;
-    return function y() {
-        console.log(a);
+// function x() {
+//     var a = 10;
+//     function y() {
+//         console.log(a);
+//     }
+//     return y;
+// }
+
+// var z = x();
+// console.log(z);
+// z();
+
+
+// // Above function is same as --->
+
+// function x() {
+//     var a = 10;
+//     return function y() {
+//         console.log(a);
+//     }
+// }
+
+// var z = x();
+// console.log(z());
+
+
+
+function z() {
+    var b = 20;
+    function x() {
+        var a = 10;
+        function y() {
+            console.log(a);
+        }
+        y();
+        console.log(b);
     }
+    x();
 }
 
-var z = x();
-console.log(z());
-
+z();
