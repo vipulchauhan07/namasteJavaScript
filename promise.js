d94565c32281e78e990e61863f09c15e397dd4b3
Vipul Chauhan <viratdev07@gmail.com>
2021-01-20 13:21:06 +0530

    Adding promise within a function and returning the promise(

--- promise.js
+++ promise.js
@@ -1,16 +1,18 @@
-var userLoggedIn = false;
+var userLoggedIn = true;
 
-var promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if(userLoggedIn) {
-            resolve();
-        } else {
-            reject();
-        }
-    }, 1000*3);
-});
+function checkUserLoggedIn() {
+    var promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(userLoggedIn) {
+                resolve("User is logged in.");
+            } else {
+                reject("User is not logged in.");
+            }
+        }, 1000*3);
+    }); 
+    return promise;
+}
 
-promise
-.then(() => console.log("User is logged in."))
-.catch(() => console.log("User is not logged in."));
+checkUserLoggedIn().then((successMsg) => console.log(successMsg))
+.catch((rejectMsg) => console.log(rejectMsg));
 
